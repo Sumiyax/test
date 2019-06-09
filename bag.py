c01458bcf111a5ac9e501973c405f8c7c1865309
Sumiyax <51506489+Sumiyax@users.noreply.github.com>
2019-06-09 17:33:21 +0800

    Delete bag.py

--- bag.py
+++
@@ -1,9 +0,0 @@
-#链接数据库
-import pandas as pd
-import pyodbc
-
-connection = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};Server=localhost;database=data;uid=sumiya;pwd=s1999211')
-query = 'select * from dbo.Sheet1$'
-
-df1 = pd.read_sql_query(query,connection)
-print(df1.head(8))
